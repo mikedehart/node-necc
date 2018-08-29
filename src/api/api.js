a4273c97486e4702f3318d369456ec91c517ea2b
Mike DeHart <mike@mrd.io>
2018-08-29 16:56:14 -0400

    Adding CSS for events and starting gallery page

---
+++ src/api/api.js
@@ -0,0 +1,3 @@
+const router = require('express').Router();
+
+// Mounting other routers for our routes (right now just for user information)
--- src/main.js
+++ src/main.js
@@ -20,4 +20,19 @@ app.get('/events', (req, res, next) => {
 	res.render('events');
 })
 
+
+app.get('/academics', (req, res, next) => {
+	res.render('events');
+})
+
+app.get('/membership', (req, res, next) => {
+	res.render('events');
+})
+
+app.get('/gallery', (req, res, next) => {
+	res.render('gallery');
+})
+
+
+
 module.exports = app;
